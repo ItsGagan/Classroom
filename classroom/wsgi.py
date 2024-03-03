490e40f78badfbf24e7ccc03a1ebe1f4b7941cb7
ItsGagan <itsgagankhatri@gmail.com>
2024-03-03 06:23:49 +0530

    Reset Wsgi.py

--- classroom/wsgi.py
+++ classroom/wsgi.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', './src/settings.py')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
 
 application = get_wsgi_application()
 
